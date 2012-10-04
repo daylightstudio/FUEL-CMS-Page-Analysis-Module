ef145edcd26393d88c9cd38fbb4b9a2ac0112a27
David McReynolds <dave@thedaylightstudio.com>
2012-10-03 17:05:12 -0700

    changed analyze button to normal button

--- views/_admin/page_analysis.php
+++ views/_admin/page_analysis.php
@@ -1,12 +1,10 @@
 <div id="fuel_main_content_inner">
 	<p class="instructions"><?=lang('page_analysis_instructions')?></p>
-	<?=site_url()?><?=$this->form->select('page', $pages_select, $this->input->post('page'))?>
-	<div style="text-align: center; margin-top: 10px;" class="buttonbar">
-		<ul>
-			<li class="end"><a href="<?=fuel_url('tools/page_analysis')?>" class="ico ico_tools_page_analysis submit_action" id="submit_page_analysis"><?=lang('btn_analyze')?></a></li>
-		</ul>
-	</div>
-	
+	<?=site_url()?><?=$this->form->select('page', $pages_select, $this->input->post('page'))?> &nbsp;
+	<?=$this->form->submit(lang('btn_analyze'), 'submit_page_analysis')?>
+	<br />
+	<br />
+	<br />
 	<div class="clear"></div>
 	
 	<?=$report?>
